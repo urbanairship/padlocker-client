4aed26a268aecc833eb986a3dc4032bcc396df90
Graham Forest <graham@urbanairship.com>
2013-02-28 22:43:38 -0800

    Add a bunch. Yep. A bunch.

--- padlocker_client/padlocker_client.py
+++ padlocker_client/padlocker_client.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import traceback
 import httplib2
 import getopt
+import socket
 import stat
 import json
 import time
@@ -41,25 +43,44 @@ client = httplib2.Http()
 def usage():
     print "usage: %s [--config=file.json] [--debug] [--help]" % os.path.basename(sys.argv[0])
 
+def deboog(msg):
+    if msg == "":
+        print u"\u250F\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u2513"
+        return
+
+    first = u"\u2503"
+    start = u"\u2523"
+    depth = (len(traceback.extract_stack()) - 3) * u"\u2501\u2501"
+    end = u"\u257E"
+    sys.stdout.write("\r%s % 5s %s%s%s %s\n" % (first, os.getpid(), start, depth, end, msg))
+    sys.stdout.write(u"\u2517\u2501\u2501\u2501\u2501\u2501\u2501\u2501\u251B")
+    sys.stdout.flush()
+
 def padlocker_post(url, data):
     """make a post request to the api_url with all relevant information"""
     full_url = "%s/%s" %(config["api_url"], url)
+    if debug:
+        deboog("POSTing %s to %s" % (json.dumps(data), full_url))
     try:
         headers, resp = client.request(
             full_url,
             "POST",
             json.dumps(data),
-            headers={'Content-type': 'application/x-www-form-urlencoded'}
+            headers={'Content-type': 'application/json'}
         )
     except httplib2.HttpLib2Error as e:
-        sys.exit(e)
+        deboog("%s gave error %s" % (full_url, e))
+        return ""
+    except socket.error, msg:
+        deboog("%s gave error %s" % (full_url, msg))
+        return ""
     if headers.status == 200:
-        return json.loads(resp)
+        if debug:
+            deboog("Got HTTP %s" % (headers.status))
+        return resp
     else:
-        sys.stderr.write("%s gave error %s\n" % (full_url, headers.status))
-        sys.stderr.write("input: %s\n" % json.dumps(data))
-        sys.stderr.write("output: %s\n" % resp)
-        return(resp, headers.status)
+        deboog("%s gave error %s\n" % (full_url, headers.status))
+        return(resp)
 
 def checkfifo(path):
     """
@@ -67,15 +88,19 @@ def checkfifo(path):
     """
 
     try:
-        if not stat.S_ISFIFO(os.stat(path).st_mode):
-            if os.path.isfile(path):
+        stat.S_ISFIFO(os.stat(path).st_mode)
+    except:
+        if os.path.isfile(path):
+            sys.stderr.write("%s exists but isn't a fifo\n" % (path, ))
+            sys.exit(1)
+        else:
+            try:
+                if debug:
+                    deboog("making fifo at %s" % (path, ))
+                os.mkfifo(path)
+            except OSError, e:
                 sys.stderr.write("can't create %s as a fifo: %s\n" % (path, e))
                 sys.exit(1)
-        else:
-                os.mkfifo(path)
-    except OSError, e:
-        sys.stderr.write("can't create %s as a fifo: %s\n" % (path, e))
-        sys.exit(1)
 
     return 1
 
@@ -94,29 +119,55 @@ def childmain(cn):
     lconfig = config["keys"][cn]
 
     if debug: 
-        print "    child: %s: %s" % (cn, lconfig)
+        deboog("%s: %s" % (cn, lconfig))
 
     checkfifo(lconfig["path"])
+
+    while 1:
+        if debug:
+            deboog("%s: waiting for %s" % (cn, lconfig["path"],))
+
+        fd = os.open(lconfig["path"], os.O_ASYNC | os.O_WRONLY)
+
+        if debug:
+            deboog("%s: %s just went writeable" % (cn, lconfig["path"],))
+
+        key = padlocker_post('', {cn: lconfig})
+
+        if key != '':
+            if debug:
+                deboog("%s: feeding key to fifo" % (cn, ))
+            os.write(fd, key)
+
+        if debug:
+            deboog("%s: closing fifo" % (cn, ))
+
+        os.close(fd)
+        time.sleep(1)
     
     sys.exit(0)
 
 
 def main():
+
+    if debug:
+        deboog("")
+
     # start a child for each config
     children = []
     for cn in config["keys"]:
-        if debug:
-            print "forking child for %s" % cn
         child = os.fork()
         if child:
             children.append(child)
+            if debug:
+                deboog("%s watcher forked as %s" % (cn, child))
         else:
             childmain(cn)
             sys.exit(0)
     for child in children:
         os.waitpid(child, 0)
     if debug:
-        print "all children died, exiting"
+        deboog("all children died, exiting")
 
 if __name__ == '__main__':
     main()
